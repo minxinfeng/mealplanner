f5ad7961939119b2b4450c864a51e6b165571e76
minxinfeng <minxinfeng@gmail.com>
2014-05-03 21:09:41 +0800

    Add test relate folders and files

---
+++ src/test/java/mealplanner/UserServiceTest.java
@@ -0,0 +1,21 @@
+package mealplanner;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.threeone.mealplanner.service.UserService;
+
+public class UserServiceTest {
+
+	@Autowired
+	private UserService userService;
+	
+	@Test
+	public void test(){
+		
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+}
